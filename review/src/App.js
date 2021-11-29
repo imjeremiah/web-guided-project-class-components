264d8290f26326406cdaade34e0733205e643370
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-29 12:25:50 -0500

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -2,16 +2,16 @@ import React, { useState } from "react";
 
 const App = () => {
   const [state, setState ] = useState({
-    name: "Allion",
+    name: "Allison",
     age: 33
   });
-  
+
   // const [name, setName ] = useState("Allison");
   // const [age, setAge ] = useState(33);
 
   return (
     <div>
-      <h1>Hello {name}. You are { age } years old.</h1>
+      <h1>Hello {state.name}. You are { state.age } years old.</h1>
       <button onClick={()=> {
         setName("Warren");
       }}>Change the name to Warren.</button>
