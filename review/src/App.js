51cec7b02d2a8b775b2cd3ae4ca5963eecefbf4f
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-29 12:22:44 -0500

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [name, setName ] = useState("Warren");
+  const [name, setName ] = useState("Allison");
+  const [age, setAge ] = useState(33);
 
   return (
     <div>
-      <h1>Hello {name}.</h1>
+      <h1>Hello {name}. You are { age } years old.</h1>
     </div>
   );
 };
