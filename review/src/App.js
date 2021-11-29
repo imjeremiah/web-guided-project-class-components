eaff5ce68cda4d24be1ccb6b03df4917575e53cd
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-29 12:28:44 -0500

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -16,6 +16,7 @@ const App = () => {
   return (
     <div>
       <h1>Hello {state.name}. You are { state.age } years old.</h1>
+      <p>{state.name} it's really good to meet you.</p>
       <button onClick={handleClick}>Change the name to Warren.</button>
     </div>
   );
