ffda9ada83722dfb5894fdd07ebd6a9889ed1d75
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-29 12:59:39 -0500

    lecture work-in-progress

--- review/src/App2.js
+++ review/src/App2.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 class App2 extends React.Component {
-    
+    constructor() {
+        super();
+        this.state = {
+            name:"Warren",
+            age: 33
+        }
+    }
+
     render() {
         const name = "Warren";          
         return(<h1>Hello {name}!</h1>);
