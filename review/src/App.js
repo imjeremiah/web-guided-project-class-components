efeb2f1a346d1fe32682d0f13eab9947e4114dc4
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-29 12:21:57 -0500

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  const name = "Chris";
+  const [name, setName ] = useState("Warren");
 
   return (
     <div>
-      <h1>Hello {name}</h1>
+      <h1>Hello {name}.</h1>
     </div>
   );
 };
