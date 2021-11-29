419bf1b28872122e867c3df8321435150276f492
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-29 13:53:05 -0500

    lecture work-in-progress

--- followAlong/src/components/Item.js
+++ followAlong/src/components/Item.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Item = props => {
   const handleClick = ()=> {
-    console.log("clicked item ", props.item.name);
+    props.handleToggleItem(props.item);
   }
 
   return (
